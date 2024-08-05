a95e742b881d8da894bc001cd61a83568e49cb7c
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2024-08-05 11:35:19 +0330

    pushing the app to the github

---
+++ cmd/web/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"chatAppGo/internal/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := routes()
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+	log.Println("Starting server on :8080")
+	_ = http.ListenAndServe(":8080", mux)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"chatAppGo/internal/handlers"
+	"github.com/bmizerany/pat"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static/", fileServer))
+	return mux
+}
